6548be2f54ac29ca09eedd6947c0dcc06f6bb693
cleitianne <oliveira.cleiti@gmail.com>
2023-07-11 20:15:22 -0300

    fixed topbar

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -22,10 +22,10 @@ const Topbar = () => {
         backgroundColor={colors.primary[400]}
         borderRadius="3px"
       >
-        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Pesquisar" />
+        {/* <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Pesquisar" />
         <IconButton type="button" sx={{ p: 1 }}>
           <SearchIcon />
-        </IconButton>
+        </IconButton> */}
       </Box>
 
       {/* ICONS */}
